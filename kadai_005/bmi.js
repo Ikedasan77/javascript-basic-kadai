b7448b74fed31315cd8e22c8ab99e14f7f01517b
Ikedasan77 <maasa1977@gmail.com>
2024-07-07 07:17:19 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,12 @@
+// JavaScriptの基礎を学ぼう_5章課題
+
+// 変数の宣言
+let bodyWeight = 68;
+
+let bodyHeight = 1.7;
+
+// bmiの計算式
+let bmi = bodyWeight /  ( bodyHeight * bodyHeight )
+
+// bmiをコンソールへ出力
+console.log( bmi );
