08a1055dc75adc33e74b766202eb31041cb76144
Ikedasan77 <maasa1977@gmail.com>
2024-07-08 00:47:28 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,22 @@
+// 変数numにランダム整数を代入する
+let num = Math.floor(Math.random() * 100) + 1; // 1から100までのランダム整数を生成
+
+// 変数(num)が3と5の倍数か判定
+if ((num % 3 === 0) && (num % 5 === 0)) {
+  console.log('3と5の倍数です');
+}
+
+// 変数(num)が15の倍数でないときの3の倍数判定
+else if (num % 3 === 0) {
+  console.log('3の倍数です');
+}
+
+// 5の倍数の判定
+else if (num % 5 === 0) {
+  console.log('5の倍数です');
+}
+
+// それ以外の場合
+else {
+  console.log(num);
+}
