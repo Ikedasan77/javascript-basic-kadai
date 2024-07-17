9d108bdf9665f80f545918535364e7d71c70c6e8
Ikedasan77 <maasa1977@gmail.com>
2024-07-17 15:31:04 +0900

    Add files via upload

--- kadai_020/kadai_020/event.js
+++ kadai_020/kadai_020/event.js
@@ -1,12 +1,15 @@
+// event.js
+
+// ページのDOMが読み込まれた後に実行する関数
 document.addEventListener('DOMContentLoaded', function() {
-    
+    // ボタン要素を取得
     var button = document.getElementById('btn');
-    
+    // テキスト要素を取得
     var textElement = document.getElementById('text');
     
-    
+    // ボタンがクリックされたときのイベントリスナーを追加
     button.addEventListener('click', function() {
-      
+      // テキストを変更
       textElement.textContent = 'ボタンがクリックされました！';
     });
   });
